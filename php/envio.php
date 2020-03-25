3c22241c12e612d6ce0247fcca093fc1346b3b60
Douglas Nunes <douglasnunes3@hotmail.com>
2020-03-25 10:09:21 -0300

    Isso não é de Deus. Eu fiz o formulário, mas acho que estou testando de forma errado por isso os erros estavam aparecendo

--- php/envio.php
+++ php/envio.php
@@ -11,20 +11,24 @@ require 'vendor/autoload.php';
 // Instantiation and passing `true` enables exceptions
 $mail = new PHPMailer(true);
 
+$nome_recebido = $_POST['nome'];
+$email_recebido = $_POST['email'];
+$assunto_recebido = $_POST['assunto'];
+$mensagem_recebida = $_POST['mensagem'];
 try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'emailremetente@gmail.com';                     // SMTP username
-    $mail->Password   = 'senha';                               // SMTP password
+    $mail->Username   = 'projetoficaemcasa@gmail.com';                     // SMTP username
+    $mail->Password   = 'ficaemcasa1.0';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
     $mail->Port       = 587;                                    // TCP port to connect to
 
     //Recipients
-    $mail->setFrom('emailremetente@gmail.com', 'Nome Remetente');
-    $mail->addAddress('emaildestinatario@gmail.com', 'Nome Destinatário');     // Add a recipient
+    $mail->setFrom($email_recebido, $nome_recebido);
+    $mail->addAddress('projetoficaemcasa@gmail.com', 'Projeto Fique em Casa');     // Add a recipient
     //$mail->addAddress('ellen@example.com');               // Name is optional
     //$mail->addReplyTo('info@example.com', 'Information');
     ///$mail->addCC('cc@example.com');
